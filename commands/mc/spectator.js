17262b2758be6d0758e153c4ef4f7b83e57d104a
Max Herold <maxoheyn@gmail.com>
2020-10-25 23:28:46 -0400

    spectator

--- commands/mc/spectator.js
+++ commands/mc/spectator.js
@@ -34,13 +34,10 @@ module.exports = class SpectatorCommand extends Command {
     }
 
     async run(msg, args) {
-        exec(`screen -S minecraft -X stuff "title ${args.mc_user} title "Given Spectator for 30 seconds.\n"`);
         exec(`screen -S minecraft -X stuff "gamemode spectator ${args.mc_user}\n"`)
-        .then(msg => {
-            msg.react('👍');
-            setTimeout(function(){
-                        exec(`screen -S minecraft -X stuff "gamemode survival ${args.mc_user}\n"`);
-            }, 30000);
-        });
+        exec(`screen -S minecraft -X stuff "title ${args.mc_user} title "Given Spectator for 30 seconds.\n"`)
+        setTimeout(function(){
+            exec(`screen -S minecraft -X stuff "gamemode survival ${args.mc_user}\n"`);
+        }, 30000);
     }
 };
