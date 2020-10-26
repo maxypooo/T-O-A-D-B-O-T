3f493f4a90b7473300859997cf7ad7fc80f00899
Max Herold <maxoheyn@gmail.com>
2020-10-26 16:15:30 -0400

    testing read chat capability

--- utility/readChat.js
+++ utility/readChat.js
@@ -4,8 +4,12 @@ const config = require('../config.json')
 
 function readChat() {
     fs.watchFile(config.minecraft.logLocation, (curr, prev) => {
-        console.log(`the current mtime is: ${curr.mtime}`);
-        console.log(`the previous mtime was: ${prev.mtime}`);
+        // console.log(`the current mtime is: ${curr.mtime}`);
+        // console.log(`the previous mtime was: ${prev.mtime}`);
+        fs.readFile(dir, 'utf-8', (err, data) => {
+            let lines = data.trim().split("\n")
+            console.log(lines[lines.length - 1])
+        });
     });
 }
 
