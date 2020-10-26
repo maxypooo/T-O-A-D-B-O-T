339b8b6652817780117507adab19c6447af24176
Max Herold <maxoheyn@gmail.com>
2020-10-26 16:36:17 -0400

    testing read chat capability

--- utility/readChat.js
+++ utility/readChat.js
@@ -1,18 +1,14 @@
 fs = require('fs');
 const config = require('../config.json')
-const {CommandoClient} = require('discord.js-commando');
-const client = new CommandoClient({
-    commandPrefix: config.prefix,
-    owner: config.owner
-});
 
-function readChat() {
+function readChat(client) {
     fs.watch(config.minecraft.logLocation, (curr, prev) => {
         // console.log(`the current mtime is: ${curr.mtime}`);
         // console.log(`the previous mtime was: ${prev.mtime}`);
         fs.readFile(config.minecraft.logLocation, 'utf-8', (err, data) => {
             let lines = data.trim().split("\n");
             let lastLine = lines[lines.length - 1];
+            console.log(`Sent message "${lastline}" to channel ${config.minecraft.chat}.`)
             client.channels.get(config.minecraft.chat).send(lastLine);
         });
     });
