098e30daf989e5f1f7b4229525a5c57a88ba801f
Max Herold <maxoheyn@gmail.com>
2020-10-26 20:26:44 -0400

    server command player list formatting

--- commands/mc/server.js
+++ commands/mc/server.js
@@ -10,35 +10,37 @@ module.exports = class ServerCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'mcserver',
-            aliases: ['mcserver', 'server'],
+            aliases: ['mcserver', 'server', 'serverstatus'],
             group: 'mc',
             memberName: 'server',
-            description: 'Minecraft Server Status',
+            description: 'Displays the Minecraft Server\'s Status',
         });
     }
 
     async run(msg, args) {
         let description = "";
-        let toadright = '<:toadright:770118404480696322>';
-        let toadleft = '<:toadleft:770118404857528360>';
         axios.get('https://api.mcsrvstat.us/2/' + config.minecraft.serverIP).then(async res => {
             // handle success
             if (res.data.online) {
-                description = `IP: ${config.minecraft.serverIP}\n`
+                description 
+                = `IP: ${config.minecraft.serverIP}\n`
                 + 'Server Status: Online' + '\n' 
+                + 'Version: ' + res.data.version + '\n'
                 + 'Player: ' + res.data.players.online + ' / ' + res.data.players.max + '\n'
-                + 'Player List: ' + res.data.players.list.toString(); + '\n'
-                + 'Last Updated: ' +  await formatTime(res.data.debug.cachetime * 1000) + '\n'
+                + 'Player List: ' + res.data.players.list + '\n'
+                + 'Last Updated: ' +  await formatTime(res.data.debug.cachetime * 1000) + '\n';
             }
             else {
-                description = `IP: ${config.minecraft.serverIP}\n` + 'Server Status: Offline' + '\n' + 'Last Updated: ' + 
-                 await formatTime(res.data.debug.cachetime * 1000) + '\n'
+                description
+                = `IP: ${config.minecraft.serverIP}\n`
+                + 'Server Status: Offline' + '\n'
+                + 'Last Updated: ' + await formatTime(res.data.debug.cachetime * 1000) + '\n';
             }
 
             /* Embed message */
             const embed = new Discord.MessageEmbed()
                 .setColor('#F56600')
-                .setTitle(`${toadright} T O A D S T A C K E R S Minecraft Server ${toadleft}`)
+                .setTitle(`${config.emojis.toadright} T O A D S T A C K E R S Minecraft Server ${config.emojis.toadleft}`)
                 .setDescription(description)
                 .setThumbnail('https://api.mcsrvstat.us/icon/' + config.minecraft.serverIP)
                 .setTimestamp();
