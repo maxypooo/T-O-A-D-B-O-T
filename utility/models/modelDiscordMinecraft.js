c2c7b88699772dd229b89453115e2faef59de3bd
Max Herold <maxoheyn@gmail.com>
2020-10-27 01:36:10 -0400

    verification test

--- utility/models/modelDiscordMinecraft.js
+++ utility/models/modelDiscordMinecraft.js
@@ -1,15 +1,24 @@
 const dynamoose = require("dynamoose");
 
 const schema = new dynamoose.Schema({
-    "discordID": {
-        "type": String,
-        "hashKey": true
+    discordID: {
+        type: String,
+        required: true,
+        hashKey: true
     },
-    "discordUser": String,
-    "minecraftUser": String
+
+    discordUser: {
+        type: String,
+        required: true
+    },
+
+    minecraftUser: {
+        type: String,
+        required: true
+    }
 });
 
 const ModelDiscordMinecraftUsers = dynamoose.model("toadbotDiscordMinecraft", schema);
 
 
-module.exports = { ModelDiscordMinecraftUsers };
+module.exports = ModelDiscordMinecraftUsers;
