2289fd91a04aefabde3cc89abb349cb007054295
Max Herold <maxoheyn@gmail.com>
2020-10-27 16:55:50 -0400

    testing verification in spec and restart

--- utility/dbUtils.js
+++ utility/dbUtils.js
@@ -2,24 +2,26 @@ const config = require('../config.json');
 const dynamoose = require("dynamoose");
 
 async function isInDB(Model, hashkey, q) {
-    const results = await Model.query(hashkey).eq(q).exec();
-    if (results[0] == undefined) {
-        console.log("User not in database.");
-    } else {
-        console.log("Success!");
-        console.log(results[0]);
-        return true;
-    }
+    await Model.query(hashkey).eq(q).exec()
+    .then(results => {
+        if (results[0] == undefined) {
+            console.log("User not in database.");
+        } else {
+            console.log("Success!");
+            console.log(results[0]);
+            return true;
+        }
+    });
 }
 
 async function getDiscordUser(Model, hashkey, discordID) {
-    const results = await Model.query(hashkey).eq(q).exec();
-    return results[0].discordUser;
+    await Model.query(hashkey).eq(q).exec()
+    .then(results => {return results[0].discordUser;});
 }
 
 async function getMinecraftUser(Model, hashkey, discordID) {
-    const results = await Model.query(hashkey).eq(q).exec();
-    return results[0].minecraftUser;
+    await Model.query(hashkey).eq(q).exec()
+    .then(results => {return results[0].minecraftUser;});
 }
 
 module.exports = { isInDB, getDiscordUser, getMinecraftUser };
