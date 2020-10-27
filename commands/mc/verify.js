8c890932179942ced5186aef6b584cbfd02c26fd
Max Herold <maxoheyn@gmail.com>
2020-10-27 01:21:51 -0400

    verification test

--- commands/mc/verify.js
+++ commands/mc/verify.js
@@ -17,6 +17,6 @@ module.exports = class VerifyCommand extends Command {
     async run(msg, args) {
         let randomString = Math.random().toString(36).substring(7);
         msg.reply(`To verify your account, type the following string EXACTLY AS SHOWN into Minecraft: ${randomString}`);
-        verifyCode(randomString);
+        verificationHandler.verifyCode(randomString);
     }
 };
