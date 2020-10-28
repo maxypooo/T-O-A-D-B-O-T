add74bea8fcc4959c2cd07e4c35d3e52e8d5d4f6
Max Herold <maxoheyn@gmail.com>
2020-10-28 03:28:53 -0400

    testing verification in spec and restart

--- commands/mc/restart.js
+++ commands/mc/restart.js
@@ -22,17 +22,25 @@ module.exports = class RestartCommand extends Command {
 
     //TODO: VERIFICATION
     async run(msg, args) {
-        if (dbUtils.isInDB(modelDiscordMinecraft, "discordID", msg.author.id)) {
-            msg.say(`${config.emojis.toadright} Minecraft Server will restart in 1 minute, check back soon. ${config.emojis.toadleft}`)
-            exec(`screen -S minecraft -X stuff "say Server restart initiated by ${msg.author.username}. Server will restart in 1 minute.\n"`);
-            setTimeout(function(){
-                exec(`screen -S minecraft -X stuff "say Server will restart in 10 seconds.\n"`);
-            }, 50000);
-            setTimeout(function(){
-                exec(`screen -S minecraft -X stuff "stop\n"`);
-            }, 60000);
-        } else {
-            msg.reply("You must first verify yourself using `toad verify` before you can use this command.")
-        }
+
+        Model.query(hashkey).eq(q).exec()
+        .then(results => {
+            if (results[0] == undefined) {
+                console.log("User not in database.");
+                msg.reply("You must first verify yourself using `toad verify` before you can use this command.")
+            } else {
+                console.log("Success! User was found.");
+                msg.say(`${config.emojis.toadright} Minecraft Server will restart in 1 minute, check back soon. ${config.emojis.toadleft}`)
+                exec(`screen -S minecraft -X stuff "say Server restart initiated by ${msg.author.username}. Server will restart in 1 minute.\n"`);
+                setTimeout(function(){
+                    exec(`screen -S minecraft -X stuff "say Server will restart in 10 seconds.\n"`);
+                }, 50000);
+                setTimeout(function(){
+                    exec(`screen -S minecraft -X stuff "stop\n"`);
+                }, 60000);
+                }
+        });
+
+            
     }
 };
