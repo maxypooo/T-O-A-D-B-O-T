36f829233025149922292faf7f17f19e93b5e3bc
Max Heyn <maxoheyn@gmail.com>
2021-02-12 21:52:53 -0500

    testing points

--- utility/points.js
+++ utility/points.js
@@ -7,8 +7,8 @@ module.exports = function() {
     this.givePointsInVoice = function(voiceChannelsArray, amountOfPoints) {
     //for (i = 0 ; i < voiceChannelsArray.length; i++) {
         //i.members.each(user => console.log(user.username));
-        console.log("username is being printed here >><M><M<<><><>")
-        voiceChannelsArray.members.each(user => console.log(user.username));
+        let voiceChannelNames = client.channels.cache.filter(ch => ch.id === voiceChannelsArray);
+        voiceChannelNames.members.each(user => console.log(user.username));
     //}
     }
 };
