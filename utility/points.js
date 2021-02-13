a0b8b4b457fb0909e495e7d730f608eb6de23f3a
Max Heyn <maxoheyn@gmail.com>
2021-02-12 21:32:10 -0500

    testing points

--- utility/points.js
+++ utility/points.js
@@ -1,9 +1,11 @@
-const { exec } = require("child_process");
+const { Command } = require('discord.js-commando');
+const config = require('../config.json')
 
 function givePointsInVoice(voiceChannelsArray, amountOfPoints) {
     //for (i = 0 ; i < voiceChannelsArray.length; i++) {
         //i.members.each(user => console.log(user.username));
-        voiceChannelsArray.members.each(user => console.log(user.username));
+        console.log("username is being printed here >><M><M<<><><>")
+        //voiceChannelsArray.members.each(user => console.log(user.username));
     //}
 }
 
