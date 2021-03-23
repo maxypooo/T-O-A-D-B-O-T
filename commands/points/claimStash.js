d696d9460f14270b7018b77dc7470c7b0d422081
BuildTools <unconfigured@null.spigotmc.org>
2021-03-22 23:12:27 -0400

    create and close gambas

--- commands/points/claimStash.js
+++ commands/points/claimStash.js
@@ -6,7 +6,7 @@ module.exports = class ClaimStashCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'claim',
-            aliases: ['stash'],
+            aliases: ['stash', "claimstash"],
             group: 'points',
             memberName: 'claimstash',
             description: 'Claim your stash once every 30 minutes.',
@@ -14,28 +14,25 @@ module.exports = class ClaimStashCommand extends Command {
                 usages: 1,
                 duration: 1800
             },
-            examples: [`${config.prefix} points`],
+            examples: [`${config.prefix} claim`],
         });
     }
 
     async run(msg) {
-        const minStash = 3;
-        const maxStash = 8;
-        const stashValue = Math.floor(Math.random() * (maxStash - minStash + 1) + minStash);
+        // Generate a random stash value between the minimum and maximum values from the config file.
+        const stashValue = Math.floor(Math.random() * (config.points.maxStash - config.points.minStash + 1) + config.points.minStash);
+
+        // Query the points database to see if the user exists.
         modelToadPoints.get({discordID: msg.author.id}, function(err, user) {
-            if (err) {
-                return console.log(err);
-            }
-            if (user == undefined) {
-                return msg.reply(`you haven't earned any ToadBucks:tm: yet. :(`)
-            }
+            if (err) { return console.log(err); }
+            if (user == undefined) { return msg.reply(`you haven't earned any ToadBucks:tm: yet. :(`); }
+
+            // If the user exists, update the database by adding the stashValue to their points value.
             modelToadPoints.update({discordID: msg.author.id}, {$ADD: {points: stashValue}}, function(err, user) {
-                if (err) {
-                    return console.log(err);
-                }
-                if (user == undefined) {
-                    return msg.reply(`you haven't earned any ToadBucks:tm: yet. You need to have earned at least 1 ToadBuck:tm: to claim a stash. :(`)
-                }
+                if (err) { return console.log(err); }
+                if (user == undefined) { return msg.reply(`you haven't earned any ToadBucks:tm: yet. You need to have earned at least 1 ToadBuck:tm: to claim a stash. :(`); }
+
+                // Let the user know that they claimed their stash and how much.
                 msg.reply(`you claimed **${stashValue}** ToadBucks:tm:! ${config.emojis.toadleft}`);
             })
         })
